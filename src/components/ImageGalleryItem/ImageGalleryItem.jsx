caf9a0d8c3ac9f7fb299c5b5dafbeb5f576807a0
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2022-01-12 22:17:40 +0200

    changes modal class to hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,31 +1,30 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import styles from './ImageGalleryItem.module.css';
-export default class ImageGalleryItem extends Component {
-    handleImageClick = event => {
+export default function ImageGalleryItem({
+    toggleModal,
+    webImage,
+    largeImage,
+    tags,
+}) {
+    const handleImageClick = event => {
         const { dataset, alt } = event.target;
-        this.props.toggleModal({
+        toggleModal({
             url: dataset.value,
             alt,
         });
     };
 
-    render() {
-        const { webImage, largeImage, tags } = this.props;
-        return (
-            <li
-                className={styles.ImageGalleryItem}
-                onClick={this.handleImageClick}
-            >
-                <img
-                    className={styles['ImageGalleryItem-image']}
-                    src={webImage}
-                    alt={tags}
-                    data-value={largeImage}
-                />
-            </li>
-        );
-    }
+    return (
+        <li className={styles.ImageGalleryItem} onClick={handleImageClick}>
+            <img
+                className={styles['ImageGalleryItem-image']}
+                src={webImage}
+                alt={tags}
+                data-value={largeImage}
+            />
+        </li>
+    );
 }
 ImageGalleryItem.propTypes = {
     toggleModal: PropTypes.func.isRequired,
