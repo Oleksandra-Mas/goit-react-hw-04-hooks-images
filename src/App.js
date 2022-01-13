acb77e3b7a683385a43eaed2735cece4991c3179
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2022-01-13 14:36:38 +0200

    moved all logic from ImageGallery to app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,93 @@
-import { useState } from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 import './App.css';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 import Modal from './components/Modal/Modal';
+import Button from './components/Button/Button';
+import Spinner from './components/Loader/Loader';
 import Searchbar from './components/Searchbar/Searchbar';
+import ApiService from './services/apiService';
+import Toast from './components/Toast/Toast';
+const apiService = new ApiService();
+
+const STATUS = {
+    idle: 'idle',
+    pending: 'pending',
+    resolved: 'resolved',
+};
+
 export default function App() {
     const [filter, setFilter] = useState('');
     const [searchImage, setSearchImage] = useState(null);
     const [showModal, setShowModal] = useState(false);
+    const [status, setStatus] = useState(STATUS.idle);
+    const [photos, setPhotos] = useState([]);
+    const [page, setPage] = useState(1);
 
     const toggleModal = data => {
         setShowModal(prevShowModal => !prevShowModal);
         setSearchImage(data);
     };
+
+    const smoothScroll = () => {
+        setTimeout(() => {
+            window.scrollBy({
+                top: document.documentElement.clientHeight,
+                behavior: 'smooth',
+            });
+        }, 400);
+    };
+
+    useEffect(() => {
+        if (!filter) {
+            return;
+        }
+        apiService.resetPage();
+        setPage(apiService.page);
+        setStatus(STATUS.pending);
+        apiService.searchQuery = filter;
+        apiService.searchPhoto().then(newPhotos => {
+            if (newPhotos.total === 0) {
+                setStatus(STATUS.idle);
+                return toast.warning('Nothing found');
+            }
+            setPhotos(newPhotos.hits);
+            setStatus(STATUS.resolved);
+        });
+    }, [filter]);
+
+    useEffect(() => {
+        if (page === 1) {
+            return;
+        }
+        setStatus(STATUS.pending);
+        apiService.searchPhoto().then(newPhotos => {
+            if (newPhotos.total === 0) {
+                setStatus(STATUS.idle);
+                return toast.warning('Nothing else found');
+            }
+            setPhotos(prevPhotos => [...prevPhotos, ...newPhotos.hits]);
+            setStatus(STATUS.resolved);
+            smoothScroll();
+        });
+    }, [page]);
+
+    const onLoadMoreClick = () => {
+        apiService.incrementPage();
+        setPage(apiService.page);
+    };
+
     return (
         <div className="App">
             <Searchbar onSubmit={setFilter} />
-            <ImageGallery toggleModal={toggleModal} filter={filter} />
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
+            {((status === STATUS.pending && page !== 1) ||
+                status === STATUS.resolved) && (
+                <ImageGallery toggleModal={toggleModal} photos={photos} />
+            )}
+            {status === STATUS.resolved && <Button onClick={onLoadMoreClick} />}
+            {status === STATUS.pending && <Spinner />}
+            <Toast />
             {showModal && (
                 <Modal searchImage={searchImage} onClose={toggleModal} />
             )}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,106 +1,32 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
-import { toast } from 'react-toastify';
 
-import Spinner from '../Loader/Loader';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import styles from './ImageGallery.module.css';
-import ApiService from '../../services/apiService';
-import Button from '../Button/Button';
-const apiService = new ApiService();
 
-const STATUS = {
-    idle: 'idle',
-    pending: 'pending',
-    resolved: 'resolved',
-};
-
-export default function ImageGallery({ toggleModal, filter }) {
-    const [status, setStatus] = useState(STATUS.idle);
-    const [photos, setPhotos] = useState([]);
-    const [page, setPage] = useState(1);
-    useEffect(() => {
-        if (!filter) {
-            return;
-        }
-        setStatus(STATUS.pending);
-        apiService.searchQuery = filter;
-        apiService.resetPage();
-        apiService.searchPhoto().then(newPhotos => {
-            if (newPhotos.total === 0) {
-                setStatus(STATUS.idle);
-                return toast.warning('Nothing found');
-            }
-            setPhotos(newPhotos.hits);
-            setStatus(STATUS.resolved);
-        });
-    }, [filter]);
-
-    useEffect(() => {
-        if (page === 1) {
-            return;
-        }
-        setStatus(STATUS.pending);
-
-        apiService.searchPhoto().then(newPhotos => {
-            if (newPhotos.total === 0) {
-                setStatus(STATUS.idle);
-                return toast.warning('Nothing else found');
-            }
-            setPhotos(prevPhotos => [...prevPhotos, ...newPhotos.hits]);
-            setStatus(STATUS.resolved);
-            smoothScroll();
-        });
-    }, [page]);
-
-    const onClick = () => {
-        apiService.incrementPage();
-        setPage(apiService.page);
-    };
-
-    const smoothScroll = () => {
-        setTimeout(() => {
-            window.scrollBy({
-                top: document.documentElement.clientHeight,
-                behavior: 'smooth',
-            });
-        }, 400);
-    };
-
-    if (status === STATUS.idle) {
-        return null;
-    }
-    if (
-        (status === STATUS.pending && page !== 1) ||
-        status === STATUS.resolved
-    ) {
-        return (
-            <>
-                <ul className={styles.ImageGallery}>
-                    {photos.map(
-                        ({ id, largeImageURL, webformatURL, tags }, index) => (
-                            <ImageGalleryItem
-                                key={index}
-                                webImage={webformatURL}
-                                largeImage={largeImageURL}
-                                tags={tags}
-                                toggleModal={toggleModal}
-                            />
-                        ),
-                    )}
-                </ul>
-                {status === STATUS.resolved ? (
-                    <Button onClick={onClick} />
-                ) : (
-                    <Spinner />
-                )}
-            </>
-        );
-    } else if (status === STATUS.pending) {
-        return <Spinner />;
-    }
+export default function ImageGallery({ toggleModal, photos }) {
+    return (
+        <>
+            <ul className={styles.ImageGallery}>
+                {photos.map(({ largeImageURL, webformatURL, tags }, index) => (
+                    <ImageGalleryItem
+                        key={index}
+                        webImage={webformatURL}
+                        largeImage={largeImageURL}
+                        tags={tags}
+                        toggleModal={toggleModal}
+                    />
+                ))}
+            </ul>
+        </>
+    );
 }
 ImageGallery.propTypes = {
     toggleModal: PropTypes.func.isRequired,
-    filter: PropTypes.string.isRequired,
+    photos: PropTypes.arrayOf(
+        PropTypes.shape({
+            largeImageURL: PropTypes.string.isRequired,
+            webformatURL: PropTypes.string.isRequired,
+            tags: PropTypes.string.isRequired,
+        }).isRequired,
+    ).isRequired,
 };
